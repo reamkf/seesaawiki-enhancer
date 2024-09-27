463a95f4b326e663bdfc3c291250259bd6d6813f
reamkf <68076678+reamkf@users.noreply.github.com>
2024-09-27 21:14:46 +0900

    Add @updateURL & @downloadURL

--- seesaawiki-enhancer.user.js
+++ seesaawiki-enhancer.user.js
@@ -6,6 +6,8 @@
 // @match        https://seesaawiki.jp/*
 // @match        https://cms.wiki.seesaa.jp/cms/*
 // @icon         https://www.google.com/s2/favicons?domain=seesaawiki.jp
+// @updateURL    https://github.com/reamkf/seesaawiki-enhancer/releases/latest/download/seesaawiki-enhancer.meta.js
+// @downloadURL  https://github.com/reamkf/seesaawiki-enhancer/releases/latest/download/seesaawiki-enhancer.user.js
 // @grant        none
 // @license      MIT
 // ==/UserScript==
