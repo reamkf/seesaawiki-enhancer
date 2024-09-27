bf7aa653d8e632724eb1745e0c196575b7717cbc
GitHub Action <action@github.com>
2024-09-27 12:23:12 +0000

    Update version to 0.0.1

--- seesaawiki-enhancer.meta.js
+++ seesaawiki-enhancer.meta.js
@@ -1,11 +1,13 @@
 // ==UserScript==
 // @name         Seesaa Wiki Enhancer
-// @version 0.0.1-test5
+// @version      0.0.1
 // @author       @_ream_kf
 // @namespace    https://seesaawiki.jp/
 // @match        https://seesaawiki.jp/*
 // @match        https://cms.wiki.seesaa.jp/cms/*
 // @icon         https://www.google.com/s2/favicons?domain=seesaawiki.jp
+// @updateURL    https://github.com/reamkf/seesaawiki-enhancer/releases/latest/download/seesaawiki-enhancer.meta.js
+// @downloadURL  https://github.com/reamkf/seesaawiki-enhancer/releases/latest/download/seesaawiki-enhancer.user.js
 // @grant        none
 // @license      MIT
 // ==/UserScript==
