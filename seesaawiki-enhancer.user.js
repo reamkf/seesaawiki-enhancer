bfc664b78943b0b11e7551752ea95abf44105311
GitHub Action <action@github.com>
2024-10-06 12:58:24 +0000

    Update version to 0.8.1

--- seesaawiki-enhancer.user.js
+++ seesaawiki-enhancer.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         Seesaa Wiki Enhancer
-// @version      0.8.0
+// @version      0.8.1
 // @author       @_ream_kf
 // @match        https://seesaawiki.jp/*
 // @match        https://*.memo.wiki/*
