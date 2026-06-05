5ab25a604584b226cedb75c881aa271715b9fd0a
reamkf <68076678+reamkf@users.noreply.github.com>
2026-06-05 20:49:53 +0900

    refactor: Change WikiPageType to enum

--- src/constants.ts
+++ src/constants.ts
@@ -1,12 +1,10 @@
-export const WikiPageType = {
-  TOP: 'TOP',
-  PAGE: 'PAGE',
-  EDIT: 'EDIT',
-  ATTACHMENT: 'ATTACHMENT',
-  LIST: 'LIST',
-  DIFF: 'DIFF',
-  HISTORY: 'HISTORY',
-  SEARCH: 'SEARCH',
-} as const;
-
-export type WikiPageType = (typeof WikiPageType)[keyof typeof WikiPageType];
+export enum WikiPageType {
+  TOP = 'TOP',
+  PAGE = 'PAGE',
+  EDIT = 'EDIT',
+  ATTACHMENT = 'ATTACHMENT',
+  LIST = 'LIST',
+  DIFF = 'DIFF',
+  HISTORY = 'HISTORY',
+  SEARCH = 'SEARCH',
+}
